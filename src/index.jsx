af9d87ad3da6b6512b56b99e7bd1413190c11939
RALD <yr.alditya@gmail.com>
2024-12-13 20:22:24 +0700

    trying2 firebase

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,7 @@
-// import React from "react";
 import { createRoot } from "react-dom/client";
-// import ContactApp from "./components/ContactApp";
-import NotesApp from "./components/Notesapp";
-
+import NotesApp from "./components/NotesApp";
 
 import './styles/style.css';
 
 const root = createRoot(document.getElementById("root"));
-// root.render(<ContactApp />);
 root.render(<NotesApp />);
